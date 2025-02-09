f7ee41b1307edad7f3f2faf9c5c5079357c63a24
3modansh <ahmad.elasayed@gmail.com>
2025-02-10 01:34:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
   const currentCategory = path.split('/search/label/')[1]; // استخراج اسم التصنيف من الرابط
 
   if (currentCategory) {
-    categoryList.style.display = 'flex'; // إظهار القائمة عند وجود تصنيف
-    categoryList.style.padding = '0.3em'; // إعادة إضافة الـ padding
-    categoryList.style.margin = '1em 0 0'; // إعادة إضافة الـ margin
+    categoryList.style.display = 'flex';
     document.querySelectorAll('.category-item a').forEach(link => {
-      // تحقق من التصنيف ومقارنته مع التصنيف الحالي في الرابط
       if (link.getAttribute('data-category') !== currentCategory) {
-        link.parentElement.style.display = 'none'; // إخفاء العناصر غير المتطابقة
+        link.parentElement.style.display = 'none'; // إخفاء غير التصنيف الحالي
       }
     });
   } else {
-    categoryList.style.display = 'none'; // إخفاء القائمة إذا لم يكن هناك تصنيف
-    categoryList.style.padding = '0'; // إخفاء الـ padding إذا لم يكن هناك تصنيف
-    categoryList.style.margin = '0'; // إخفاء الـ margin إذا لم يكن هناك تصنيف
+    categoryList.style.display = 'none';
   }
 });
