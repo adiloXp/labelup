5db777db834cc9ce32f6d7f5a8ff31d7f2d20a50
3modansh <ahmad.elasayed@gmail.com>
2025-02-10 01:24:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
   if (!categoryList) return;
 
   const path = window.location.pathname;
-
-  // إظهار القائمة فقط في الصفحة الرئيسية
-  if (path === '/' || path === '/index.html' || path === '/home') {
-    categoryList.style.display = 'flex';
-  } else {
-    categoryList.style.display = 'none'; // إخفاء القائمة في الصفحات الأخرى
-  }
-
   const currentCategory = path.split('/search/label/')[1]; // استخراج اسم التصنيف من الرابط
 
+  // إذا كان الرابط يحتوي على تصنيف (مثل /search/label/{category})
   if (currentCategory) {
+    categoryList.style.display = 'flex'; // إظهار القائمة
     document.querySelectorAll('.category-item a').forEach(link => {
+      // تحقق من التصنيف ومقارنته مع التصنيف الحالي في الرابط
       if (link.getAttribute('data-category') !== currentCategory) {
         link.parentElement.style.display = 'none'; // إخفاء العناصر غير المتطابقة
       }
     });
+  } else {
+    categoryList.style.display = 'none'; // إخفاء القائمة إذا لم يكن هناك تصنيف
   }
 });
